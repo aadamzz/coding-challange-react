48fe264e364daf7b873d9e48a4efc22367c24cb2
Adam Zubrzycki <az.zubrzycki99@gmail.com>
2020-05-17 22:47:50 +0200

    Updates

---
+++ precache-manifest.c23fb99924e924abce20e671d77c59b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b930e5fdd2b2519952f195fde96eede1",
+    "url": "/github.io/coding-challange-react/index.html"
+  },
+  {
+    "revision": "4e53181611fde8c912fc",
+    "url": "/github.io/coding-challange-react/static/js/2.0b7c9c7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/coding-challange-react/static/js/2.0b7c9c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac5dec9f89fc71f8cf9",
+    "url": "/github.io/coding-challange-react/static/js/main.ebcf2929.chunk.js"
+  },
+  {
+    "revision": "77d92a104c777a88c259",
+    "url": "/github.io/coding-challange-react/static/js/runtime-main.891fca97.js"
+  }
+]);
